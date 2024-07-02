4974dd9b1e3e2103aabd30b0849cab9bbb21141a
e-nie <eniehaus111@gmail.com>
2024-07-02 15:49:33 +0200

    add .env to gitignore

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -2,14 +2,14 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Authentication & Authorization ', () => {
     test.beforeEach(async ({ page }) => {
-        await page.goto('https://coding.pasv.us/user/login');
+        await page.goto('/user/login');
 
     });
 
     test('Sign in with existing credentials', async ({ page }) => {
 
-        await page.locator('#normal_login_email').fill('vl1vl@yahoo.com');
-        await page.locator('#normal_login_password').fill('57ThTRTV99qf!5L');
+        await page.locator('#normal_login_email').fill(process.env.EMAIL);
+        await page.locator('#normal_login_password').fill(process.env.PASSWORD);
         await page.locator('button[type="submit"]').click();
 
         await expect(page.locator('.ant-avatar-square')).toBeVisible();
